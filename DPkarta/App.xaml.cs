3beed482334b2350f8b50ed108edaf8c3fa12d80
senar.libor@proton.me <senar.libor@proton.me>
2024-10-14 11:32:36 +0200

    Add messaging and screen brightness features
    Modified App.xaml.cs to include CommunityToolkit.Mvvm.Messaging and added OnResume and OnSleep methods to send messages using WeakReferenceMessenger. Updated DPkarta.csproj to remove conditional target framework for Windows, update package versions, and add Plugin.Maui.ScreenBrightness. Enhanced MainPage.xaml.cs to handle screen brightness changes based on messages, streamline login/logout process, and remove redundant code. Refactored MauiProgram.cs to streamline builder configuration.

--- DPkarta/App.xaml.cs
+++ DPkarta/App.xaml.cs
@@ -1,12 +1,26 @@
-﻿namespace DPkarta
+﻿using CommunityToolkit.Mvvm.Messaging;
+namespace DPkarta
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
         }
+        protected override void OnResume()
+        {
+            WeakReferenceMessenger.Default.Send(new Message() { message = "resume" });
+            base.OnResume();
+        }
+        protected override void OnSleep()
+        {
+            WeakReferenceMessenger.Default.Send(new Message() { message = "sleep" });
+            base.OnSleep();
+        }
+    }
+    public class Message()
+    {
+        public required string message { get; set; }
     }
 }
--- DPkarta/MainPage.xaml.cs
+++ DPkarta/MainPage.xaml.cs
@@ -3,19 +3,35 @@ using ZXing.Common;
 using System.Text.Json;
 using System.Net;
 using System.Text;
-
+using CommunityToolkit.Mvvm.Messaging;
+using Plugin.Maui.ScreenBrightness;
 namespace DPkarta
 {
     public partial class MainPage : ContentPage
     {
+        string loginURI = "https://m.dpmhk.qrbus.me/api/auth/signIn";
+        public float normalBrightness = 1;
         const int dpmhkID = 3903132;
         string snr = "";
         Services services = new();
         public MainPage()
         {
             InitializeComponent();
+            WeakReferenceMessenger.Default.Register<DPkarta.Message>(this, (sender, args) =>
+            {
+                switch (args.message)
+                {
+                    case "resume":
+                        normalBrightness = ScreenBrightness.Default.Brightness;
+                        ScreenBrightness.Default.Brightness = 1;
+                        LoadImage();
+                        break;
+                    case "sleep":
+                        ScreenBrightness.Default.Brightness =normalBrightness;
+                        break;
+                }
+            });
         }
-
         private void OnLoginClicked(object sender, EventArgs e)
         {
             if (SecureStorage.GetAsync("user").Result != null)
@@ -24,28 +40,9 @@ namespace DPkarta
                 SecureStorage.Remove("user");
                 ChangeScreens(true);
                 Button.Text = "Login";
+                ScreenBrightness.Default.Brightness = normalBrightness;
                 return;
             }
-
-
-            try
-            {
-                if (SecureStorage.GetAsync("user").Result != null)
-                {
-                    //logging out
-                    SecureStorage.Remove("user");
-                    ChangeScreens(true);
-                    Button.Text = "Login";
-                    return;
-                }
-            }
-            catch (Exception)
-            {
-
-            }
-
-
-            string loginURI = "https://m.dpmhk.qrbus.me/api/auth/signIn";
 
             var json = services.Post(loginURI, $"{{\"organizationSystemEntityId\": {dpmhkID},\"login\":\"{UsernameEntry.Text}\",\"password\":\"{PasswordEntry.Text}\"}}");
             if (json == "notfound")
@@ -58,7 +55,6 @@ namespace DPkarta
                 DisplayAlert("Error", "Something went wrong, please contant developer", "OK");
                 return;
             }
-
             if (json.Contains("loginError.password") || json.Contains("loginError.login"))
             {
                 DisplayAlert("Error", "Wrong password", "OK");
@@ -75,8 +71,18 @@ namespace DPkarta
 
             switch (user)
             {
-                case { wertyzUser.cards.Length: 1 }: snr = user.wertyzUser.cards[0].snr; SecureStorage.SetAsync("user", snr); ChangeScreens(false); Button.Text = "Logout"; LoadImage(); break;
-                case { wertyzUser.cards.Length: > 1 }: CardPicker.ItemsSource = user.wertyzUser.cards.Select(c => c.ownerFirstName + " " + c.ownerLastName).ToList(); CardPicker.IsVisible = true; break;
+                case { wertyzUser.cards.Length: 1 }:
+                    snr = user.wertyzUser.cards[0].snr;
+                    SecureStorage.SetAsync("user", snr);
+                    ChangeScreens(false); Button.Text = "Logout";
+                    LoadImage();
+                    normalBrightness = ScreenBrightness.Default.Brightness;
+                    ScreenBrightness.Default.Brightness = 1;
+                    break;
+                case { wertyzUser.cards.Length: > 1 }:
+                    CardPicker.ItemsSource = user.wertyzUser.cards.Select(c => c.ownerFirstName + " " + c.ownerLastName).ToList();
+                    CardPicker.IsVisible = true;
+                    break;
                 default: DisplayAlert("Error", "No card found", "OK"); break;
             }
         }
@@ -90,9 +96,10 @@ namespace DPkarta
                 ChangeScreens(false);
                 Button.Text = "Logout";
                 LoadImage();
+                normalBrightness = ScreenBrightness.Default.Brightness;
+                ScreenBrightness.Default.Brightness = 1;
             }
         }
-
         protected override void OnAppearing()
         {
             try
@@ -104,6 +111,8 @@ namespace DPkarta
                     ChangeScreens(false);
                     Button.Text = "Logout";
                     LoadImage();
+                    normalBrightness = ScreenBrightness.Default.Brightness;
+                    ScreenBrightness.Default.Brightness = 1;
                 }
             }
             catch (Exception)
@@ -116,7 +125,6 @@ namespace DPkarta
             }
 
         }
-
         private void LoadImage()
         {
             string cardURI = "https://m.dpmhk.qrbus.me/api/rest/publiccards/encryptCardData";
@@ -165,9 +173,7 @@ namespace DPkarta
             PasswordEntry.IsVisible = isLogin;
             RefreshButton.IsVisible = !isLogin;
         }
-
         private void RefreshButton_Clicked(object sender, EventArgs e) => LoadImage();
-
 
     }
 }
